fd2a33fa5fafcdf872d65b86b7e846572f644a19
MayorChano <dan@dancodes.online>
2019-05-21 14:04:39 +0200

    double quotes

--- src/server.js
+++ src/server.js
@@ -1,39 +1,39 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
-const helmet = require('helmet');
+const createError = require("http-errors");
+const express = require("express");
+const path = require("path");
+const session = require("express-session");
+const bodyParser = require("body-parser");
+const cookieParser = require("cookie-parser");
+const logger = require("morgan");
+const helmet = require("helmet");
 
-const database = require('./utils/handlers/database');
+const database = require("./utils/handlers/database");
 
-const indexRouter = require('./routes/index');
-const restApi = require('./routes/api/v1/index');
+const indexRouter = require("./routes/index");
+const restApi = require("./routes/api/v1/index");
 
 const app = express();
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "ejs");
 
 const cooky = {
-	secret: 'work hard',
-  	resave: true,
-  	expires: new Date() * 60 * 60 * 24 * 7,
-  	saveUninitialized: true
-}
-app.set('trust proxy', 1);
+	secret: "work hard",
+  resave: true,
+  expires: new Date() * 60 * 60 * 24 * 7,
+  saveUninitialized: true
+};
+app.set("trust proxy", 1);
 app.use(helmet());
 app.use(session(cooky));
-app.use(logger('tiny'));
+app.use(logger("tiny"));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use('/', indexRouter);
-app.use('/api', restApi);
+app.use("/", indexRouter);
+app.use("/api", restApi);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
@@ -43,10 +43,10 @@ app.use(function(req, res, next) {
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
+  res.locals.error = req.app.get("env") === "development" ? err : {};
 
   // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render("error");
 });
 module.exports = app;
