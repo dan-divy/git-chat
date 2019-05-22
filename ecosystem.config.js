419e8fa828cf749efbeb31b0e3d29b806df787ff
MayorChano <dan@dancodes.online>
2019-05-22 21:19:44 +0200

    eco-system

---
+++ ecosystem.config.js
@@ -0,0 +1,10 @@
+const config = require('./package.json')
+module.exports = {
+  apps : [{
+    name: config.name,
+    script: './bin/www',
+    instances: 1,
+    autorestart: true,
+    max_memory_restart: '1G'
+  }]
+}
