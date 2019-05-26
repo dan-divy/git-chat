bd8f7e62d8793becc0c3471b7f246c6ecbeb499a
MayorChano <dan@dancodes.online>
2019-05-26 19:25:13 +0200

    omg no

--- src/config/config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    config: true,
-    github:{
-        clientID: "fb99c686920a399896d3",
-        clientSecret: "aeee34e36b13a23c3cdb1c55ebfe2d7ab599cfde",
-        callbackUrl:"http://localhost:8080/authorize"
-    }
-}
