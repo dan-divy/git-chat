eb0fa14be2d0f793b6b28a220fe37ee29f6985ab
MayorChano <dan@dancodes.online>
2019-06-02 16:05:41 +0200

    ignore for linguist

--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const get = require('got');
+const fetch = require('axios');
 
 router.all("/*", (req, res, next) => {
 	if(!req.session.user) {
@@ -12,8 +12,8 @@ router.all("/*", (req, res, next) => {
 
 router.get("/user/repos", (req, res) => {
 	console.log(req.session.user.repos_url)
-	get(req.session.user.repos_url, {json:true}).then(response => {
-		return res.send(response.body);
+	fetch(req.session.user.repos_url).then(response => {
+		return res.send(response.data);
 	});
 });
 
