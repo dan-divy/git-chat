a047ecafdccc9da00fcd7bf6170d33b8c6928d2b
MayorChano <dan@dancodes.online>
2019-06-02 15:53:36 +0200

    get repos in api

--- src/routes/api/v1/index.js
+++ src/routes/api/v1/index.js
@@ -1,10 +1,20 @@
 const express = require("express");
 const router = express.Router();
+const get = require('got');
 
 router.all("/*", (req, res, next) => {
 	if(!req.session.user) {
-		return res.status(404).send({error: "session", msg: "User is not logged in");
+		return res.status(404).send({error: "session", msg: "User is not logged in"});
+	} else {
+		next();
 	}
-})
+});
+
+router.get("/user/repos", (req, res) => {
+	console.log(req.session.user.repos_url)
+	get(req.session.user.repos_url, {json:true}).then(response => {
+		return res.send(response.body);
+	});
+});
 
 module.exports = router;
