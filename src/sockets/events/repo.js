bdbc2ddd5f8a8760cf260dc863acb39bd0bd9998
Dan <dan@dancodes.online>
2019-07-16 17:08:00 +0200

    repo errors

--- src/sockets/events/repo.js
+++ src/sockets/events/repo.js
@@ -4,17 +4,40 @@ const db = require("../../utils/handlers/database");
 const { q } = require("../../server");
 
 module.exports = function(socket, data) {
-  q.push(function(cb) {
-    fetch({
-      method: "GET",
-      headers: {
-        Authorization: "token " + socket.session.user.accessToken
-      },
-      url: `https://api.github.com/repos${data}`
-    }).then(response => {
+  fetch({
+    method: "GET",
+    headers: {
+      Authorization: "token " + socket.session.user.accessToken
+    },
+    url: `https://api.github.com/repos${data}`
+  })
+    .then(response => {
       const repo = response.data;
       console.log(repo);
+      db.get({ value: { id: repo.id } }, function(err, found) {
+        if (found) {
+          return socket.emit("found", found.value.full_name);
+        }
+        if (!repo.push) {
+          return socket.emit("err", {
+            title: "You do not have permission",
+            sub:
+              "To initialize a repo in Git-Chat you need <b>push</b> permissions."
+          });
+        }
+        if (repo.private) {
+          return socket.emit("err", {
+            title: "No repo found",
+            sub: "There is not repo found or it is private."
+          });
+        }
+      });
+      console.log(repo.id);
+    })
+    .catch(err => {
+      socket.emit("err", {
+        title: "No repo found",
+        sub: "There is not repo found or it is private."
+      });
     });
-  });
-  q.on("success", function(result, job) {});
 };
